7dde243a652363e3f0a4fdd860fdc5cc830a5de7
djmbritt <david@effect.ai>
2022-09-23 13:21:47 +0100

    update config

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
   },
 
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+  ssr: true,
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
